b306b04f3f32a8d0f36a128690823e04075706d5
chetan <chetan.sheshagiri@gmail.com>
2018-08-24 16:43:30 +0530

    My project setup is completed

---
+++ ecommerce-backend/src/main/java/com/chetan/ecommerce_backend/App.java
@@ -0,0 +1,13 @@
+package com.chetan.ecommerce_backend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
